1c2ae513d98745240bdbfed4a21d6f3fd339755e
ricarthlima <rrsl@cin.ufpe.br>
2019-08-03 09:43:50 +0000

    modal de login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,9 @@
 class PagesController < ApplicationController
+    def index
+        if user_signed_in? then
+            render :app #redirect_to app_path
+        else
+            render :index
+        end
+    end
 end
