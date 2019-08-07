d63c4a2496cc6d993051bda60cf1e8fe706c531d
ricarthlima <rrsl@cin.ufpe.br>
2019-08-07 14:10:35 +0000

    close #8 #10

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module Workspace
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.i18n.default_locale = :'pt-BR'        
   end
 end
+
+
